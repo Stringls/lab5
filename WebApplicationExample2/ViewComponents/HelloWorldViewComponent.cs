8834ca49b866fe80baea037b2e2de9e001add3c4
Artie <temasad12@gmail.com>
2023-12-26 12:02:00 +0200

    Add a View component

---
+++ WebApplicationExample2/ViewComponents/HelloWorldViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationExample2.ViewComponents
+{
+    public class HelloWorldViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            // You can perform any logic here if needed
+            // For simplicity, let's just return a simple message
+            return Content("Hello, World!");
+        }
+    }
+}
